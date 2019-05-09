bc1313855d58c28fa031200eaf6aa1e553e637de
shiqiupp <pping555pp@163.com>
2019-05-09 23:19:32 +0800

    _5_LongestPalindromicSubstring with dp

--- src/main/java/com/beta/study/leetcode/_5_LongestPalindromicSubstring.java
+++ src/main/java/com/beta/study/leetcode/_5_LongestPalindromicSubstring.java
@@ -18,24 +18,51 @@ package com.beta.study.leetcode;
  */
 public class _5_LongestPalindromicSubstring {
     public static void main( String[] args ) {
-        String str = "";
+        String str = "abaccdcc";
+
+        System.out.println(longestPalindrome_dp( str ) );
 
     }
 
-    public static String longestPalindrome( String s ) {
+    /**
+     * dp 解法
+     * 设置一个二维数组 dp[i][j] , 表示子串i~j是一个回文串
+     * 那么，dp[i-1][j+1] = s[i-1] == s[j+1] && dp[i][j] == true
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome_dp( String s ) {
         String result = "";
 
-        if ( s == null || s.length( ) == 0 ) {
-            result = "";
+        if ( s == null || s.length( ) <= 1 ) {
+            result = s;
         } else {
-            int maxLength = 0;
-            for ( int i = 0 ; i < s.length( ) ; i++ ) {
-                char c = s.charAt( i );
-
-                for ( int j = i + 1 ; j < s.length( ) ; j++ ) {
-                    char b = s.charAt( j );
+            int length = s.length( );
+            boolean[][] dp = new boolean[ length ][ length ];
+            for ( int i=0;i<length;i++ ) {
+                dp[i][i] = true;
+            }
+            int maxLength = 0 , begin = 0;
+            for ( int i = 1 ; i < length ; i++ ) {
+                for ( int j = 0 ; j < i ; j++ ) {
+                    if ( s.charAt( i ) == s.charAt( j ) )  {
+                        if ( (i-1) >= 0 && (j+1) < length && (dp[j+1][i-1]  || (i-j == 1))) {
+                            dp[j][i] = true;
+                           if ( maxLength < (i-j+1) ) {
+                               maxLength = i-j+1;
+                               begin = j;
+                           }
+                        }
+                    }
 
                 }
+            }
+
+            if ( maxLength == 0 && begin == 0 ) {
+                result = s.charAt( 0 ) + "";
+            }else {
+                result = s.substring( begin , begin + maxLength );
             }
         }
 
