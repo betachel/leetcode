85e115ec5ce992324d7f3ad8dd73c046af14a33c
Betachel <pping555pp@163.com>
2020-07-12 17:59:11 +0800

    27

--- src/main/java/com/beta/study/leetcode/_31_NextPermutation.java
+++ src/main/java/com/beta/study/leetcode/_31_NextPermutation.java
@@ -15,36 +15,38 @@ import java.util.stream.Stream;
  * 1,3,2 ¡ú 2,1,3
  * 3,2,1 ¡ú 1,2,3
  * 1,1,5 ¡ú 1,5,1
+ *
  * @author shiqiu
  * @date 2019/07/02
  */
 public class _31_NextPermutation {
-    public static void main( String[] args ) {
-        int[ ] nums = new  int[]{ 1,1,1,2};
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 1, 1, 2};
 
-        nextPermutation( nums );
+        nextPermutation(nums);
 
-        for ( int i=0;i<nums.length;i++ ) {
-            System.out.print( nums[i]);
-            if ( i+1 != nums.length ) {
-                System.out.print( " , " );
+        for ( int i = 0; i < nums.length; i++ ) {
+            System.out.print(nums[i]);
+            if ( i + 1 != nums.length ) {
+                System.out.print(" , ");
             }
         }
     }
+
     public static void nextPermutation(int[] nums) {
         if ( nums != null && nums.length > 0 ) {
             int length = nums.length;
-            int last = nums[length-1];
-            int i = length -2;
-            for (  ; i>=0;i-- ) {
+            int last = nums[length - 1];
+            int i = length - 2;
+            for ( ; i >= 0; i-- ) {
                 if ( nums[i] < last ) {
-                    int temp =  nums[length-1];
-                    nums[length-1] = nums[i];
+                    int temp = nums[length - 1];
+                    nums[length - 1] = nums[i];
                     nums[i] = temp;
 
-                    if ( i > 0 && nums[i-1] < nums[i] ) {
-                         temp =  nums[i-1];
-                        nums[i-1] = nums[i];
+                    if ( i > 0 && nums[i - 1] < nums[i] ) {
+                        temp = nums[i - 1];
+                        nums[i - 1] = nums[i];
                         nums[i] = temp;
                         break;
                     }
@@ -54,8 +56,8 @@ public class _31_NextPermutation {
             }
             if ( i < 0 ) {
                 i = 0;
-                while ( i < (length - i - 1) ) {
-                    int temp =  nums[i];
+                while ( i < ( length - i - 1 ) ) {
+                    int temp = nums[i];
                     nums[i] = nums[length - i - 1];
                     nums[length - i - 1] = temp;
                     i++;
