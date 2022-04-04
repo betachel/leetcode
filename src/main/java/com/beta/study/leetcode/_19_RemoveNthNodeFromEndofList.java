5f347e8dbc105323f33b899100f351ca3e9cf0df
betachel <pping555pp@163.com>
2022-04-04 22:09:02 +0800

    practice

--- src/main/java/com/beta/study/leetcode/_19_RemoveNthNodeFromEndofList.java
+++ src/main/java/com/beta/study/leetcode/_19_RemoveNthNodeFromEndofList.java
@@ -2,18 +2,17 @@ package com.beta.study.leetcode;
 
 /**
  * Given a linked list, remove the n-th node from the end of list and return its head.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Given linked list: 1->2->3->4->5, and n = 2.
- *
- * After removing the second node from the end, the linked list becomes 1->2->3->5.
- * Note:
- *
+ * <p>
+ * After removing the second node from the end, the linked list becomes 1->2->3->5. Note:
+ * <p>
  * Given n will always be valid.
- *
+ * <p>
  * Follow up:
- *
+ * <p>
  * Could you do this in one pass?
  *
  * @author shiqiu
@@ -24,56 +23,67 @@ public class _19_RemoveNthNodeFromEndofList {
         int val;
         ListNode next;
 
-        ListNode( int x ) {
+        ListNode(int x) {
             val = x;
         }
     }
 
-    public static void main( String[] args ) {
-        ListNode head =init( 5 );
+    public static void main(String[] args) {
+        ListNode head = init(5);
 
-        ListNode result = removeNthFromEnd(head , 5);
+        ListNode result = removeNthFromEnd(head, 5);
 
-        while ( result != null ) {
-            System.out.print( result.val + " -> " );
-            result = result.next;
-        }
-        System.out.println( );
+       print(result);
+
+        head = init(5);
+        result = removeNthFromEnd_v2(head, 2);
+
+        print(result);
     }
 
-    public static ListNode init( int length) {
-        ListNode head = new  ListNode( 1 );
+    private static void print(ListNode head) {
         ListNode temp = head;
-        int i=1;
-        while ( i++ < length ) {
-            temp.next = new ListNode( i );
+
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
+
+    public static ListNode init(int length) {
+        ListNode head = new ListNode(1);
+        ListNode temp = head;
+        int i = 1;
+        while (i++ < length) {
+            temp.next = new ListNode(i);
             temp = temp.next;
         }
 
         return head;
     }
 
-
-    public static ListNode removeNthFromEnd( ListNode head , int n ) {
-        ListNode resultHead = null ;
-        ListNode temp = head ;
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode resultHead = null;
+        ListNode temp = head;
 
         int length = 0;
-        while ( temp != null ) {
+        while (temp != null) {
             temp = temp.next;
             length++;
         }
 
-        if ( length > 0 && n <= length ) {
-            if ( n == length ) {
+        if (length > 0 && n <= length) {
+            if (n == length) {
                 resultHead = head.next;
             } else {
                 int index = length - n, i = 1;
                 resultHead = head;
                 temp = head.next;
                 ListNode pre = head;
-                while ( temp != null ) {
-                    if ( i++ == index ) {
+                while (temp != null) {
+                    if (i++ == index) {
                         pre.next = temp.next;
                         break;
                     } else {
@@ -84,8 +94,34 @@ public class _19_RemoveNthNodeFromEndofList {
             }
         }
 
-
         return resultHead;
+    }
+
+    /**
+     * 采用快慢指针
+     * @param head
+     * @param n
+     * @return
+     */
+    public static ListNode removeNthFromEnd_v2(ListNode head, int n) {
+
+        int count = 0;
+        ListNode fast = head;
+        while (count < n && fast != null) {
+            fast = fast.next;
+            count++;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        ListNode slow = head;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+
+        return head;
     }
 
 }
