3eb8743c39e46b9e2d56b6fbfe7cce4093a287f8
betachel <pping555pp@163.com>
2022-04-05 10:40:17 +0800

    practice

--- src/main/java/com/beta/study/leetcode/_142_LinkedListCycleII.java
+++ src/main/java/com/beta/study/leetcode/_142_LinkedListCycleII.java
@@ -6,20 +6,26 @@ import com.beta.study.leetcode.model.ListNode;
  * @author shiqiu
  * @date 2022/04/05
  */
-public class _141_LinkedListCycle {
-    public boolean hasCycle(ListNode head) {
+public class _142_LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
         /**
          * 快慢指针，一个一次走一步，一个一次走两步，能相遇说明有环，否则无环
+         * 看解释：https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/linked-list-cycle-ii-kuai-man-zhi-zhen-shuang-zhi-/
          */
-        if (head == null) {return false;}
+        if (head == null) {return null;}
         ListNode slow = head, fast = head;
         while (slow != null && fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                return true;
+                break;
             }
         }
-        return false;
+        fast = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
     }
 }
