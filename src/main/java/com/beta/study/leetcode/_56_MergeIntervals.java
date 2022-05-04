a273e5267311747bc5d56e85dcaebaacd772549d
betachel <pping555pp@163.com>
2022-05-04 22:13:58 +0800

    合并区间

--- src/main/java/com/beta/study/leetcode/_56_MergeIntervals.java
+++ src/main/java/com/beta/study/leetcode/_56_MergeIntervals.java
@@ -1,10 +1,8 @@
 package com.beta.study.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * @author shiqiu
@@ -13,42 +11,44 @@ import java.util.Set;
 public class _56_MergeIntervals {
 
     public static void main(String[] args) {
-        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-        _56_MergeIntervals mergeIntervals = new _56_MergeIntervals();
-        int[][] result = mergeIntervals.merge(intervals);
-        //for (int i = 0; i < result.length; i++) {
-        //    System.out.println(result[i][0] + " " + result[i][1]);
-        //}
+        int[][] intervals = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+        _56_MergeIntervals instance = new _56_MergeIntervals();
+        int[][] result = instance.merge(intervals);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i][0] + " " + result[i][1]);
+        }
     }
 
     public int[][] merge(int[][] intervals) {
         if (intervals == null || intervals.length == 0) {
             return intervals;
         }
-        int n = intervals.length;
-        Map<Integer, Set<Integer>> map = new java.util.HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int[] interval = intervals[i];
-            for (int j = 0; j < interval.length; j++) {
-                Set<Integer> set = map.getOrDefault(interval[j], new java.util.HashSet<>());
-                set.add(i);
-                map.put(interval[j], set);
-            }
-        }
-        List<Set<Integer>> result = new ArrayList<>();
-        map.forEach((k, v) -> {
-            Set<Integer> set = new HashSet<>();
-            result.add(set);
-            for(int i: v) {
-                int[] interval = intervals[i];
-                for(int value : interval) {
-                    if(value != k) {
-                        set.add(value);
-                    }
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        List<int[]> list = new LinkedList<>();
+        int row = intervals.length;
+        for (int i = 0; i < row; ) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int j = i + 1;
+            for (; j < row; j++) {
+                if (end < intervals[j][0]) {
+                    break;
                 }
+                end = Math.max(end, intervals[j][1]);
             }
+            i = j;
+            int[] array = new int[] {start, end};
+            list.add(array);
+        }
 
-        });
-        return null;
+        int[][] array = new int[list.size()][2];
+        int i = 0;
+        while (i < list.size()) {
+            int[] temp = list.get(i);
+            array[i][0] = temp[0];
+            array[i][1] = temp[1];
+            i++;
+        }
+        return array;
     }
 }
